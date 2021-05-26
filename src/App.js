bf514db05601063ebeddaeec5cf4d25ce4f930b8
Yesum Lee <yesum0314@gmail.com>
2021-05-26 12:33:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const Statistic = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.number}</td>
+    </tr>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0); // 갯수
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // 평균 = (가격 * 갯수 + 가격 * 갯수 ...) / 총 갯수 (갯수 + 갯수 + 갯수)
+  const setToGoodScore = () => {
+    setGood(good + 1);
+  };
+
+  const setToNeutralScore = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const setToBadScore = () => {
+    setBad(bad + 1);
+  };
+
+  const all = good + neutral + bad;
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={() => setToGoodScore()} text="good" />
+      <Button handleClick={() => setToNeutralScore()} text="neutral" />
+      <Button handleClick={() => setToBadScore()} text="bad" />
+      <h2>Statistics</h2>
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <div>No feedback given</div>
+      ) : (
+        <>
+          <Statistic number={good} text="good" />
+          <Statistic number={neutral} text="neutral" />
+          <Statistic number={bad} text="bad" />
+          <Statistic number={all} text="all" />
+          <Statistic
+            number={(1 * good + 0 * neutral + -1 * bad) / all}
+            text="average"
+          />
+          <Statistic number={(good / all) * 100} text="positive" />
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
